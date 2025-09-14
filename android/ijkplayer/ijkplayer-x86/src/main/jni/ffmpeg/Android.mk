4b24813bfbef81c9ac44e11cd719ee936abf3349
vFISHv <vfishv@gmail.com>
2025-09-14 19:41:11 +0800

    16KB page sizes

--- android/ijkplayer/ijkplayer-x86/src/main/jni/ffmpeg/Android.mk
+++ android/ijkplayer/ijkplayer-x86/src/main/jni/ffmpeg/Android.mk
@@ -3,4 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ijkffmpeg
 LOCAL_SRC_FILES := $(MY_APP_FFMPEG_OUTPUT_PATH)/libijkffmpeg.so
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+LOCAL_LDFLAGS += "-Wl,-z,common-page-size=16384"
 include $(PREBUILT_SHARED_LIBRARY)
